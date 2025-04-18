b8e7d32b3b8a51c7db754b1394e4f10dfffd8022
kw520 <1151094976@qq.com>
2025-04-18 20:51:50 +0800

    pinia base

--- src/views/layout/LayoutIndex.vue
+++ src/views/layout/LayoutIndex.vue
@@ -2,6 +2,16 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+
+import {useCategoryStore} from "@/stores/category.js";
+import {onMounted} from "vue";
+
+const categoryStore = useCategoryStore();
+
+onMounted(() => {
+    categoryStore.getCategory()
+})
+
 </script>
 
 <template>
