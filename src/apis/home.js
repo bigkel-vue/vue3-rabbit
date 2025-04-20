7d1d581c6a9fa428ecc9b2895ace03582db56ec2
kw520 <1151094976@qq.com>
2025-04-20 15:18:26 +0800

    img lazy

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,19 @@ export function getBannerApi() {
         url:"home/banner"
     })
 }
+
+// 定义函数的两种方式
+export const findNewApi = () => {
+    return httpInstance({
+        url:"home/new"
+    })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+    return  httpInstance.get('home/hot')
+}
